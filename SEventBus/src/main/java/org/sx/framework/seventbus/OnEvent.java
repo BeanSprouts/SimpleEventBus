771a1d71eb533765c245c15823abee621ad25c00
shen xin <shenxin@outlook.com>
2015-07-23 11:00:35 +0800

    注解增加继承支持

--- SEventBus/src/main/java/org/sx/framework/seventbus/OnEvent.java
+++ SEventBus/src/main/java/org/sx/framework/seventbus/OnEvent.java
@@ -2,6 +2,7 @@ package org.sx.framework.seventbus;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -11,6 +12,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
 public @interface OnEvent {
     ThreadMode threadMode() default ThreadMode.Async;
